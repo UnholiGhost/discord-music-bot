82cbacb8c6f91365f002ddb1d4dbd70ed4cab15f
UnholiGhost <dietrichkmnnen@gmail.com>
2023-10-03 20:30:53 +0200

    Added Removal of Queue Entries

---
+++ commands/commands/music/shift.js
@@ -0,0 +1,65 @@
+import * as env from 'dotenv';
+import { SlashCommandBuilder } from 'discord.js';
+import { GuildMember } from 'discord.js';
+
+const notificationPrefix = env.config().parsed.NOTIFICATION_PREFIX;
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('shift')
+    .setDescription('Remove the next entry from the queue.'),
+  async execute(interaction, player) {
+    try {
+      if (
+        !interaction.member.voice.channel ||
+        !(interaction.member instanceof GuildMember)
+      ) {
+        return interaction.reply({
+          content: `${notificationPrefix} Warning: you've got to be in a voice channel to use this command.`,
+          ephemeral: true
+        });
+      }
+
+      if (
+        interaction.guild.members.me.voice.channelId &&
+        interaction.member.voice.channelId !==
+          interaction.guild.members.me.voice.channelId
+      ) {
+        return void interaction.reply({
+          content: `${notificationPrefix} Warning: you're not in the right voice channel.`,
+          ephemeral: true
+        });
+      }
+
+      await interaction.deferReply();
+
+      const queue = player.nodes.get(interaction.guildId);
+
+      if (!queue || !queue?.node.isPlaying())
+        return void interaction.followUp({
+          content: `${notificationPrefix} Error: no music's being played.`
+        });
+
+      if (queue.tracks.length < 1) {
+        return void interaction.followUp({
+          content: `${notificationPrefix} Warning: the queue is empty.`,
+          ephemeral: true
+        });
+      }
+
+      const nestTrack = queue.tracks.data[0];
+      const success = queue.node.remove(nestTrack);
+
+      return void interaction.followUp({
+        content: success
+          ? `${notificationPrefix} Notification: removed '${nestTrack}' from the queue.`
+          : `${notificationPrefix} Notification: something's gone wrong.`
+      });
+    } catch (err) {
+      console.log(`${notificationPrefix} Error: ${err}`);
+      interaction.followUp({
+        content: `${notificationPrefix} Error: ${err.message}`
+      });
+    }
+  }
+};
